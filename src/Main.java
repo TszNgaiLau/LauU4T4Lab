86246ae7f89bd9f4ac34939ca1b35a3083df4acd
BT_2N2_08 <sample@bths.edu>
2023-11-08 13:41:10 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args)
+    {
+        int result = 0;
+        int k = 0;
+        while (k < 4) {
+            int m = 0;
+            while (m < 3) {  // nested while loop
+                result++;
+                m++;
+                System.out.println("k " + k + ", m " + m);
+            }
+            k++;
+        }
+        System.out.println("result: " + result);
+
+    }
 }
+
